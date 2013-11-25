c0bc29fcd688baab350a95d85c27a704e1019666
cedy <yurii.lesh@gmail.com>
2013-11-25 15:16:11 -0500

    raw working script

---
+++ routerRestart.py
@@ -0,0 +1,58 @@
+import urllib.request
+import urllib.error
+import socket
+
+
+class routeRestart:
+    '''Little script for reboot router ZOOm 5352 \r\n 
+    Usage: script.py ip login password \r\n
+    Example: script.py 192.168.0.1 admin password'''
+    
+    def __init__(self, ip, login, password):
+        self.ip = ip
+        self.login = login
+        self.password = password
+        
+    def singup(self):
+        login_data = {"loginUsername" : self.login,
+                      "loginPassword" : self.password}
+        login_data = urllib.parse.urlencode(login_data).encode('utf8')
+        url = "http://{0}/goform/login".format(self.ip)
+        #logining
+        req = urllib.request.Request(url, login_data)
+        try:
+            urllib.request.urlopen(req, timeout=3)#.read().decode('utf8')
+        except urllib.error.URLError:
+            print("Server doesn't respond")
+            exit(1)
+            
+    def reboot(self):
+        url = "http://{0}/goform/RgSecurity".format(self.ip)
+        reboot_data = {"UserId" : '',
+                       "OldPassword" : '',
+                       "Password" : '',
+                       "PasswordReEnter" : '',
+                       "ResRebootYes" : '0x01', 
+                       "RestoreFactoryNo" : '0x00', 
+                       "RgRouterMode" : 1}
+        reboot_data = urllib.parse.urlencode(reboot_data).encode('utf8')
+        req = urllib.request.Request(url, reboot_data)
+        try:
+            urllib.request.urlopen(req, timeout=3)
+        except urllib.error.URLError:
+            print("Server doesn't respond2")
+            exit(1)
+        except socket.timeout:
+            print("Restarting")
+            
+            
+            
+def main():
+    ex = routeRestart("192.168.0.1", 'admi', 'admin')
+    ex.singup()
+    ex.reboot()
+if __name__ == '__main__':
+    main()
+    
+    
+    
